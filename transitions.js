5aab1b66965c73ad30871720c7623c2ea3bd8dde
Carpmenter <nickcarpenter1017@gmail.com>
2019-03-05 16:16:00 -0600

    fixing transition

--- transitions.js
+++ transitions.js
@@ -1,28 +1,29 @@
+$(document).ready(function () {
 
+    $("nav li a").on('click', function(event){
 
-$("nav li a").on('click', function(event){
-
-    event.preventDefault();
-
-    const link = $(this).attr("href");
-    console.log(link);
-
-    window.history.pushState(null, null, link);
-
-    $("nav li a").removeClass('active');
-    $(this).addClass('active');
-
-    $.ajax({
-        url: link,
-        success: function(data) {
-            $('main').fadeOut(250, function() {
-
-                var page = $(data).filter('main').html();
-                
-                $('main').html(page);
-                $('main').fadeIn(250); 
-            });
-        }
-        
+        event.preventDefault();
+    
+        const link = $(this).attr("href");
+        console.log(link);
+    
+        window.history.pushState(null, null, link);
+    
+        $("nav li a").removeClass('active');
+        $(this).addClass('active');
+    
+        $.ajax({
+            url: link,
+            success: function(data) {
+                $('main').fadeOut(250, function() {
+                    var page = $(data).filter('main').html();
+                    
+                    $('main').html(page);
+                    $('main').fadeIn(250); 
+                });
+            }
+            
+        });
     });
+
 });
